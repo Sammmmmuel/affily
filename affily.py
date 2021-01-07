b497436e804f95f74facc538ea84e6f64b0d2f42
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-01-07 12:18:41 -0800

    registration works

--- affily.py
+++ affily.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "13443423234234"
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 
-
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="register", form=form)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", title="login", form=form)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
