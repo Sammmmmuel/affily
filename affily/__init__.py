e75e94b62e73ea350ba49d3fe93eea1a769e5423
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-01-11 11:33:59 -0800

    authentication working

--- affily/__init__.py
+++ affily/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
@@ -7,5 +9,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "13443423234234"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from affily import routes
